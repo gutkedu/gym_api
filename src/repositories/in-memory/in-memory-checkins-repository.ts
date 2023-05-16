b81b6ffc28c15e12c08678a4b56545ae4e1509d7
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-05-15 23:08:52 -0300

    test: check ins

--- src/repositories/in-memory/in-memory-checkins-repository.ts
+++ src/repositories/in-memory/in-memory-checkins-repository.ts
@@ -1,9 +1,33 @@
 import { Prisma, CheckIn } from '@prisma/client'
 import { ICheckInsRepository } from '../check-ins-repository'
 import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckInssRepository implements ICheckInsRepository {
   public items: CheckIn[] = []
+
+  async findByUserIdOnDate(
+    userId: string,
+    date: Date,
+  ): Promise<CheckIn | null> {
+    const startOfTheDay = dayjs(date).startOf('date')
+    const endOfTheDay = dayjs(date).endOf('date')
+
+    const checkInOnSameDate = this.items.find((checkIn) => {
+      const checkInDate = dayjs(checkIn.created_at)
+
+      const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+      return checkIn.user_id === userId && isOnSameDate
+    })
+
+    if (!checkInOnSameDate) {
+      return null
+    }
+
+    return checkInOnSameDate
+  }
 
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn = {
--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 
 import { ICheckInsRepository } from '@/repositories/check-ins-repository'
 import { CheckInUseCase } from './check-in'
@@ -11,6 +11,11 @@ describe('Check in use case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInssRepository()
     sut = new CheckInUseCase(checkInsRepository)
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
@@ -22,5 +27,39 @@ describe('Check in use case', () => {
     expect(checkIn.id).toEqual(expect.any(String))
     expect(checkIn.gym_id).toEqual('gym-id')
     expect(checkIn.user_id).toEqual('user-id')
+  })
+
+  it('should not be able to check in twice on the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id',
+    })
+
+    await expect(async () => {
+      await sut.execute({
+        gymId: 'gym-id',
+        userId: 'user-id',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should not be able to check in twice on different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id',
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-id',
+      userId: 'user-id',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
   })
 })
