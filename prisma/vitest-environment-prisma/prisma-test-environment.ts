399f7ca488068650858006eb37316f517a973733
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-05-25 21:28:09 -0300

    feat: vitest test environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,12 +1,40 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string): string {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('Executing vitest environment setup')
+    const schema = randomUUID()
+
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.databaseURL = databaseURL
+
+    execSync('npx prisma migrate deploy')
+
     return {
-      teardown() {
-        console.log('Executing vitest environment teardown')
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
