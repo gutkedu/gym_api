9fd1c20ec8ca5520a9abaee4ce230d4ab2ecf852
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-05-25 22:08:54 -0300

    feat: register e2e test and fix in prisma test env

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+
 import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
 import { execSync } from 'node:child_process'
@@ -6,9 +7,9 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-function generateDatabaseURL(schema: string): string {
+function generateDatabaseURL(schema: string) {
   if (!process.env.DATABASE_URL) {
-    throw new Error('Please provide a DATABASE_URL environment variable')
+    throw new Error('Please provide a DATABASE_URL environment variable.')
   }
 
   const url = new URL(process.env.DATABASE_URL)
@@ -22,10 +23,9 @@ export default <Environment>{
   name: 'prisma',
   async setup() {
     const schema = randomUUID()
-
     const databaseURL = generateDatabaseURL(schema)
 
-    process.env.databaseURL = databaseURL
+    process.env.DATABASE_URL = databaseURL
 
     execSync('npx prisma migrate deploy')
 
@@ -34,6 +34,7 @@ export default <Environment>{
         await prisma.$executeRawUnsafe(
           `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
         )
+
         await prisma.$disconnect()
       },
     }
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from 'vitest'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should register a user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: 'senha@12345!',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
