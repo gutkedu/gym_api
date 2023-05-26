885acbdfc56ecef7e68163c9363b45da5fa0053d
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-05-25 23:36:43 -0300

    test: check in e2e tests

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Create check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('it should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        name: 'Javascript Gym',
+        latitude: -27.1,
+        longitude: -49.64,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -27.1,
+        longitude: -49.64,
+      })
+
+    expect(response.statusCode).toBe(201)
+  })
+})
