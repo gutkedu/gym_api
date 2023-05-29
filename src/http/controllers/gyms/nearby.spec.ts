bdbe0588cb28ee315001c5222124e849c5fc9df9
Eduardo Gutkoski <eduardo.pedogutkoski@gmail.com>
2023-05-28 21:38:27 -0300

    test: pass tests for role based auth

--- src/http/controllers/gyms/nearby.spec.ts
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -13,7 +13,7 @@ describe('Nearby Gyms (e2e)', () => {
   })
 
   it('it should be able search nearby gyms', async () => {
-    const { token } = await createAndAuthenticateUser(app)
+    const { token } = await createAndAuthenticateUser(app, true)
 
     await request(app.server)
       .post('/gyms')
--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,19 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  const user = await request(app.server).post('/users').send({
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    password: 'senha@12345!',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('senha@12345!', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const authResponse = await request(app.server).post('/sessions').send({
@@ -17,6 +25,5 @@ export async function createAndAuthenticateUser(app: FastifyInstance) {
 
   return {
     token,
-    user,
   }
 }
